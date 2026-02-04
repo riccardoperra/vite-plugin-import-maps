d8d4a14ae381ba518825f39b38a93afcf1c118e6
riccardoperra <riccardo.perra@icloud.com>
2026-02-04 20:59:43 +0100

    fix tests

---
+++ .changeset/fast-carpets-clap.md
@@ -0,0 +1,5 @@
+---
+"vite-plugin-native-import-maps": minor
+---
+
+Add support for vite7. SSR improvements
--- test/__snapshot__/build-project-with-right-import-maps/assets/@import-maps/shared-lib-BulYBtFC.js
+++ test/__snapshot__/build-project-with-right-import-maps/assets/@import-maps/shared-lib-BulYBtFC.js
@@ -2,12 +2,7 @@ function foo() {
   return "test";
 }
 const bar = "bar";
-const sharedLib = {
-  foo,
-  bar
-};
 export {
   bar,
-  sharedLib as default,
   foo
 };
